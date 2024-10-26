3b14c0985c9e7eb2cfb502ec17c20eda6fdca196
Ibrahim Itani <I.itani2002@gmail.com>
2024-10-26 14:17:36 +0300

    completed exercise in python

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+# O(rows),rows being the input given by user
 def printDiamond(rows):
+    # O(rows), rows being the number of iterations based on input
     for i in range(1,rows+1):
         # print(" "*(rows-i)+"*"*((2*i)+1)) starts from 3 *
         print(" "*(rows-i)+"*"*((2*i)-1))  #starts from 1 *
+        
+    # O(rows-1), rows-1 being the number of iterations based on input
+    for i in  range(rows-1,0,-1):
+        # the middle row will be deleted since rows-i =0
+        print(" "*(rows-i)+"*"*((2*i)-1))
         
     
 def main():
