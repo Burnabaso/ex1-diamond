7775d664d9f2802f115157e60845c81453a6fa38
Ibrahim Itani <I.itani2002@gmail.com>
2024-10-26 14:49:44 +0300

    tested .repeat method in js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,12 @@
 function printDiamonds(rows){
-    console.log("Hi",rows)
+    var row = "";
+    // for(var i = 1;i <= rows;i++)
+    // {
+    //     row +=
+    // }
 }   
 (function (){
-    const userInput = prompt("Enter number of rows: ");
-    printDiamonds(userInput);
+    // const userInput = prompt("Enter number of rows: ");
+    // printDiamonds(userInput);
+    console.log("Hi".repeat(2));
 })();
