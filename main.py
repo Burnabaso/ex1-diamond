2ac91dae00fab821829a32131357c826104b3264
Ibrahim Itani <I.itani2002@gmail.com>
2024-10-26 14:10:53 +0300

    fixed upper part printing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 def printDiamond(rows):
     for i in range(1,rows+1):
-        print(" "*(rows-i)+"*"*((2*i)+1))
+        # print(" "*(rows-i)+"*"*((2*i)+1)) starts from 3 *
+        print(" "*(rows-i)+"*"*((2*i)-1))  #starts from 1 *
         
     
 def main():
