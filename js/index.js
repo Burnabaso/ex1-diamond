b1b61f4a5675b2f3697e3b4cb15c5b56208a2ac1
Ibrahim Itani <I.itani2002@gmail.com>
2024-10-26 14:53:47 +0300

    implemented upper part based on py code

--- js/index.js
+++ js/index.js
@@ -1,12 +1,13 @@
 function printDiamonds(rows){
     var row = "";
-    // for(var i = 1;i <= rows;i++)
-    // {
-    //     row +=
-    // }
+    for(var i = 1;i <= rows;i++)
+    {
+        row += " ".repeat(rows-i)+ "*".repeat((2*i)-1) +"\n"
+    }
+    console.log(row);
 }   
 (function (){
     // const userInput = prompt("Enter number of rows: ");
-    // printDiamonds(userInput);
-    console.log("Hi".repeat(2));
+    printDiamonds(4);
+   
 })();
