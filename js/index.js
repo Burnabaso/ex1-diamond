cbe267f8d6414d7b6b97788e557fc1a15fb3ac4a
Ibrahim Itani <I.itani2002@gmail.com>
2024-10-26 14:59:32 +0300

    implemented lower part based  on python code

--- js/index.js
+++ js/index.js
@@ -1,7 +1,17 @@
+// O(rows),rows being the user input
 function printDiamonds(rows){
     var row = "";
+    // upper part
+    // O(rows), rows being the number of iterations based on input
     for(var i = 1;i <= rows;i++)
     {
+        // accumulate rows of upper part in row
+        row += " ".repeat(rows-i)+ "*".repeat((2*i)-1) +"\n"
+    }
+    // lower part
+    // O(rows-1), rows being the user input
+    for(var i =rows-1;i>0;i--){
+        // add the lower part to rows
         row += " ".repeat(rows-i)+ "*".repeat((2*i)-1) +"\n"
     }
     console.log(row);
